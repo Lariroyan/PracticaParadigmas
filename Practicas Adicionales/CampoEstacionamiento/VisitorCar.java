23c3af96c4344442927b5d5d2ad9c5f86afc5cc3
Lari <94306835+Lariroyan@users.noreply.github.com>
2024-12-07 21:56:55 -0300

    Update VisitorCar.java

--- Practicas Adicionales/CampoEstacionamiento/VisitorCar.java
+++ Practicas Adicionales/CampoEstacionamiento/VisitorCar.java
@@ -1,5 +1,19 @@
 package estacionamiento;
 
-public class VisitorCar extends ParkedCar{
-    private int
+public abstract class ParkedCar {
+    protected Car car;
+
+    public ParkedCar(Car car) {
+        this.car = car;
+    }
+
+    public abstract int fee();
+
+    public int passengers() {
+        return car.getPassengers();
+    }
+
+    public boolean holds(Car earlyCar) {
+        return car == earlyCar;
+    }
 }
