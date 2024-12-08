5afc42b5016d901a5a9c44a89b0d07f94c4b897f
Lari <94306835+Lariroyan@users.noreply.github.com>
2024-12-07 21:58:09 -0300

    Update Garage.java

--- Practicas Adicionales/CampoEstacionamiento/Garage.java
+++ Practicas Adicionales/CampoEstacionamiento/Garage.java
@@ -1,36 +1,43 @@
 package estacionamiento;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Garage {
-    private int totalGuests = 0;
-    public int futureFees;
+    private List<ParkedCar> cars = new ArrayList();
+
+    public Garage() {
+    }
 
     public int futureFees() {
-        return futureFees;
+        return (Integer)this.cars.stream().reduce(0, (total, car) -> {
+            return total + car.fee();
+        }, Integer::sum);
     }
 
     public void parkMemberCar(Car car) {
-        totalGuests += car.getPassengers();
-        futureFees += 100;
-        
+        this.cars.add(new MemberCar(car));
     }
 
     public int guests() {
-        return totalGuests;
+        return (Integer)this.cars.stream().reduce(0, (total, car) -> {
+            return total + car.passengers();
+        }, Integer::sum);
     }
 
-
-    public void unpark(Car car) {
-        totalGuests -= car.getPassengers();
+    public void unpark(Car earlyCar) {
+        this.cars.removeAll((Collection)this.cars.stream().filter((parked) -> {
+            return parked.holds(earlyCar);
+        }).collect(Collectors.toList()));
     }
 
     public void parkVisitorCar(Car car) {
-        totalGuests += car.getPassengers();
-        futureFees += 50 + (car.getPassengers() * 60);
-
+        this.cars.add(new VisitorCar(car));
     }
 
     public void parkInvitedCar(Car car) {
-        totalGuests += car.getPassengers();
-        futureFees += 150;
+        this.cars.add(new InvitedCar(car));
     }
 }
