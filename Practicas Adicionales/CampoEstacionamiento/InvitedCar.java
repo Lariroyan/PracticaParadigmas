aa04e24996d817c6157b55492d5fe6bcf1f77dd3
Lari <94306835+Lariroyan@users.noreply.github.com>
2024-12-07 21:56:25 -0300

    Update InvitedCar.java

--- Practicas Adicionales/CampoEstacionamiento/InvitedCar.java
+++ Practicas Adicionales/CampoEstacionamiento/InvitedCar.java
@@ -1,4 +1,15 @@
 package estacionamiento;
 
 public class InvitedCar extends ParkedCar{
+    public static int InvitedFeeCar = 50;
+    public static int InvitedFeePassenger = 60;
+
+    public InvitedCar(Car car){
+        super(car);
+    }
+
+    @Override
+    public int fee() {
+        return InvitedFeeCar +  InvitedFeePassenger*passengers();
+    }
 }
