dc54048979105b34e2747a34aea1bdc6e5f6f4bf
Lari <94306835+Lariroyan@users.noreply.github.com>
2024-12-09 13:10:41 -0300

    Update CajeroTest.java

--- Practicas Adicionales/Cajero/CajeroTest.java
+++ Practicas Adicionales/Cajero/CajeroTest.java
@@ -7,54 +7,69 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CajeroTest {
+
+    public static final String JUAN = "Juan";
+    public static final String ROMI = "Romi";
+    public static final String CARLOS = "Carlos";
+    public static final String LUCIA = "Lucia";
+    public static final String MONI = "Moni";
+
     @Test void test01ElPozoEsCeroSiNoHayApuestas(){
-        Quiniela quiniela = new Quiniela();
-        assertEquals(0, quiniela.getPozo());
+        assertEquals(0, new Quiniela().getPozo());
     }
     @Test void test02ApuestaUnNumeroALaCabeza(){
-        Quiniela quiniela = new Quiniela();
-        quiniela.registrarApuestaCabeza("Juan", 1234, 100);
+        Quiniela quiniela = QuinielaConApuestaALaCabeza();
         quiniela.cerrarApuesta(Arrays.asList(1234, 5678, 9012, 3456, 7890));
-        assertEquals(100, quiniela.getPozo());
-        assertEquals(2000, quiniela.consultarPremio("Juan"));
+        assertPozoAndPremio(100, quiniela, 2000, JUAN);
     }
+
     @Test void tes03ApuestaALosPrimerosCinco(){
         Quiniela quiniela = new Quiniela();
-        quiniela.registrarApuestaALosPrimerosCinco("Romi",  9012, 100);
+        quiniela.registrarApuestaALosPrimerosCinco(ROMI,  9012, 100);
         quiniela.cerrarApuesta(Arrays.asList(1234, 5678, 9012, 3456, 7890));
-        assertEquals(100, quiniela.getPozo());
-        assertEquals(1400, quiniela.consultarPremio("Romi"));
+        assertPozoAndPremio(100, quiniela, 1400, ROMI);
     }
     @Test void test04ApuestaALosPrimerosDiez(){
         Quiniela quiniela = new Quiniela();
-        quiniela.registrarApuestaLosPrimerosDiez("Carlos",  1212, 20);
+        quiniela.registrarApuestaLosPrimerosDiez(CARLOS,  1212, 20);
 
         quiniela.cerrarApuesta(Arrays.asList(1234, 5678, 9012, 3456, 7890, 2345, 6789, 1010, 1212, 3434));
-        assertEquals(20, quiniela.getPozo());
-        assertEquals(200, quiniela.consultarPremio("Carlos"));
+        assertPozoAndPremio(20, quiniela, 200, CARLOS);
     }
     @Test void test05ApuestaALaCentena(){
         Quiniela quiniela = new Quiniela();
-        quiniela.registrarApuestaALaCentena("Lucia", 234 , 100);
-
+        quiniela.registrarApuestaALaCentena(LUCIA, 234 , 100);
         quiniela.cerrarApuesta(Arrays.asList(1234, 5678, 9012, 3456, 7890)); // 5678 tiene centena 678
-        assertEquals(900, quiniela.consultarPremio("Lucia"));
+        assertPozoAndPremio(100, quiniela, 900, LUCIA);
 
     }
     @Test void test06ApuestaALaDecena(){
-        Quiniela quiniela = new Quiniela();
-        quiniela.registrarApuestaALaDecena("Moni", 34, 10);
+        Quiniela quiniela = QuinielaConApuestaALaDecena();
         quiniela.cerrarApuesta(Arrays.asList(1234, 5678, 9012, 3456, 7890));
-        assertEquals(20, quiniela.consultarPremio("Moni"));
+        assertEquals(20, quiniela.consultarPremio(MONI));
     }
     @Test void test07ApuestasCombinadasALaDecenaYALaCabeza(){
-        Quiniela quiniela = new Quiniela();
-        quiniela.registrarApuestaALaDecena("Moni", 34, 10);
-        quiniela.registrarApuestaCabeza("Juan", 1234, 100);
+        Quiniela quiniela = QuinielaConApuestaALaDecena();
+        quiniela.registrarApuestaCabeza(JUAN, 1234, 100);
         quiniela.cerrarApuesta(Arrays.asList(1234, 5678, 9012, 3456, 7890));
-        assertEquals(110, quiniela.getPozo());
-        assertEquals(20, quiniela.consultarPremio("Moni"));
-        assertEquals(2000, quiniela.consultarPremio("Juan"));
+        assertPozoAndPremio(110, quiniela, 20, MONI);
+        assertEquals(2000, quiniela.consultarPremio(JUAN));
+    }
+
+    private static Quiniela QuinielaConApuestaALaDecena() {
+        Quiniela quiniela = new Quiniela();
+        quiniela.registrarApuestaALaDecena(MONI, 34, 10);
+        return quiniela;
+    }
+
+    private static Quiniela QuinielaConApuestaALaCabeza() {
+        Quiniela quiniela = new Quiniela();
+        quiniela.registrarApuestaCabeza(JUAN, 1234, 100);
+        return quiniela;
+    }
+    private static void assertPozoAndPremio(int monto, Quiniela quiniela, int premio, String nombre) {
+        assertEquals(monto, quiniela.getPozo());
+        assertEquals(premio, quiniela.consultarPremio(nombre));
     }
 
 }
